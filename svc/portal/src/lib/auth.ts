3b09cb6e3f241fc3af1a33435ab295224ef99d83
Kasper Jønsson <kas@lerpz.com>
2025-10-14 20:23:52 +0200

    Always ask for account

--- svc/portal/src/lib/auth.ts
+++ svc/portal/src/lib/auth.ts
@@ -1,18 +1,18 @@
 import NextAuth from "next-auth";
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 
-export const {
-  handlers,
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET!,
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_CLIENT_ID!,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_CLIENT_SECRET!,
       issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
     }),
   ],
 });
