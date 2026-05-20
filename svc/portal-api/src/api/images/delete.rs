afb02d916809bdcbf0b02194d64b44a0e0007e13
Kasper Jønsson <kas@lerpz.com>
2026-05-20 15:10:56 +0200

    feat(images): add delete endpoint

--- svc/portal-api/src/api/images/delete.rs
+++ svc/portal-api/src/api/images/delete.rs
@@ -1,6 +1,15 @@
-use lerpz_axum::problem::{HandlerResult, ProblemSchema};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+};
+use lerpz_axum::problem::{HandlerResult, Problem, ProblemSchema};
+use lerpz_metadata::{MetadataClient, MetadataKind, models::StorageMetadata};
+use uuid::Uuid;
 
-use crate::oapi::IMAGES_TAG;
+use crate::{
+    oapi::IMAGES_TAG,
+    state::{AppState, DatabasePool, S3Client},
+};
 
 #[utoipa::path(
     method(delete),
@@ -37,6 +46,64 @@ use crate::oapi::IMAGES_TAG;
     ),
 )]
 #[axum::debug_handler(state = AppState)]
-pub async fn handler() -> HandlerResult<()> {
+pub async fn handler(
+    State(database): State<DatabasePool>,
+    State(s3): State<S3Client>,
+    Path(id): Path<Uuid>,
+) -> HandlerResult<()> {
+    let meta_client = lerpz_metadata::Client::from_pool(database);
+
+    tracing::trace!(id = %id, "fetching metadata from database");
+    let metadata = meta_client
+        .delete(id, MetadataKind::Image)
+        .await
+        .map_err(|err| match err {
+            lerpz_metadata::error::Error::NotFound(_) => Problem::new(
+                StatusCode::NOT_FOUND,
+                "Not Found",
+                "The requested image was not found.",
+            ),
+            err => Problem::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal Server Error",
+                err.to_string(),
+            ),
+        })?;
+
+    let (bucket, key) = match metadata {
+        lerpz_metadata::Metadata::Image { storage, .. } => match storage {
+            StorageMetadata::S3 { bucket, key } => (bucket, key),
+            StorageMetadata::ABS { .. } => {
+                return Err(Problem::new(
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Internal Server Error",
+                    "Unsupported storage backend for image deletion.",
+                ));
+            }
+        },
+        _ => {
+            return Err(Problem::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal Server Error",
+                "Unexpected metadata kind returned for image.",
+            ));
+        }
+    };
+
+
+    tracing::trace!(bucket = %bucket, key = %key, "deleting image from storage (s3)");
+    s3.delete_object()
+        .bucket(&bucket)
+        .key(&key)
+        .send()
+        .await
+        .map_err(|err| {
+            Problem::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal Server Error",
+                err.to_string(),
+            )
+        })?;
+
     Ok(())
 }
