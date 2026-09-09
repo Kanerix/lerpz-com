0984e73d5fd35e2e7fd50f72b8104f3d2bf6ee8e
Kasper Jønsson <daekjo@egmont.com>
2026-09-09 11:26:08 +0200

    build: add a devenv development shell

---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{ pkgs, lib, config, inputs, ... }:
+
+let
+  systemPackages = with pkgs; [
+    openssl
+    pkg-config
+  ];
+in
+{
+  languages.rust = {
+    enable = true;
+    toolchainFile = ./rust-toolchain.toml;
+  };
+
+  languages.javascript = {
+    enable = true;
+    bun.enable = true;
+    bun.install.enable = true;
+  };
+
+  packages =
+    (with pkgs; [
+      just
+      sqlx-cli
+      mkcert
+      kubectl
+      kubernetes-helm
+      kind
+      terraform
+    ])
+    ++ systemPackages
+    ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+
+  env.SQLX_OFFLINE = "true";
+
+  enterShell = ''
+    echo "rust $(rustc --version | cut -d' ' -f2) · bun $(bun --version)"
+    echo "run 'just' to list recipes"
+  '';
+
+  enterTest = ''
+    rustc --version
+    bun --version
+    just --version
+  '';
+}
